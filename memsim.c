03d4d415bc87dfcaee9222143818715c6bac10f6
HgSwift <38018337+HgSwift@users.noreply.github.com>
2020-02-29 09:24:07 -0500

    Added LRU
    Not tested due to faulty internet.
    Methodology:
    Iterates through the file with a while loop. Every time the loop iterates the int count is increased. Whenever a frame is used the value of this integer is assigned to a different array(recent) at the exact position of the modified frame. Instead of the int evict being randomly assigned a for loop will go through the entire recent array, find the smallest integer and set the value of evict to that integer.

--- memsim.c
+++ memsim.c
@@ -12,7 +12,7 @@ typedef struct page{
     random page replacement function
 */
 void RDM(char* filename, int frame_size, char* mode);
-
+void LRU(char* filename, int frame_size, char* mode);
 
 int main(int argc, char* argv[]){
     // check to make sure proper amount of arguments were provided to the program
@@ -148,6 +148,163 @@ void RDM(char* filename, int frame_size, char* mode){
                 newPage.address = addr;
                 newPage.dirtyBit = 1;
                 // if the random page to be evicted has been modified
+                // increment writes and read
+                if(frames[evict].dirtyBit == 1){
+                    writes++;
+                    reads++;
+                    frames[evict] = newPage;
+                }else{
+                    // if the page to be evicted has not beed modified, just increment reads
+                    reads++;
+                    frames[evict] = newPage;
+                }
+            }
+        }
+
+
+    }
+    fclose(file);
+    printf("Total memory frames: %d\n", frame_size);
+    printf("Events in trace: %d\n", events);
+    printf("Total disk reads: %d\n", reads);
+    printf("Total disk writes: %d\n", writes);
+}
+
+void LRU(char* filename, int frame_size, char* mode){
+    /*
+        Function to manually replace pages within computer RAM
+        and allocate necessary bits to creation of new pages based on running
+        program memory needs 
+    */
+
+   /*
+   Iterates through the file and every line the int count grows
+   when a frame is used the same position in a different array
+   is assigned the value of count. Meaning that the least recently
+   used frame will have the same positon as the lowest value in the
+   recent array.   
+   */
+    srand(time(0));
+    Page frames[frame_size];
+    int count = -1;
+    int recent[frame_size];
+
+    // initialize the pages to some null values
+    for(int i = 0; i<frame_size; i++){
+        frames[i].address = -1;
+        frames[i].dirtyBit = -1;
+    }
+    unsigned addr;
+    char rw;
+    int reads = 0, writes = 0, events = 0;
+    FILE* file = fopen(filename, "r");
+    int previous_page = 0;
+    bool found;
+    while(fscanf(file,"%x %c",&addr,&rw) != EOF){
+        count++;
+        if(strcmp(mode, "debug") == 0){
+            printf("Performing Event: %d, %c, %d\n", events, rw, addr);
+        }
+
+
+        
+        found = false;
+        // immediately increment the events variable if we find another trace to read
+        events++;
+
+        // if a read event is happening
+        if(rw == 'R'){
+            // check the previously accessed frame first
+            if(frames[previous_page].address == addr){
+                found = true;
+                recent[previous_page] = count;
+                continue;
+            }
+
+            for(int i = 0; i < frame_size; i++){
+                // if we find the relevant page, continue
+                // and set previous page to the index we found it at
+                // after this set the corresponding value in recent to count
+                // this is to take advantage of principle of locality
+                // to avoid iterating through the entire array for multiple
+                // reads of the same page
+                if(frames[i].address == addr){
+                    previous_page = i;
+                    recent[previous_page] = count;
+                    found = true;
+                    break;
+                }
+            }
+
+            // if the page was not found search for least recently used page.
+            if(!found){
+                int comp = count;
+                int evict;
+                // Iterates through recent to find the position of the lowest value
+                for(int j = 0; j<frame_size;j++){
+                    if(recent[j] < comp){
+                        evict = j;
+                    }
+                }
+
+                //int evict = rand() % frame_size;
+                Page newPage;
+                newPage.address = addr;
+                // if the random page to be evicted has been modified
+                // increment writes and read
+                if(frames[evict].dirtyBit == 1){
+                    writes++;
+                    reads++;
+                    frames[evict] = newPage;
+                }else{
+                    // if the page to be evicted has not beed modified, just increment reads
+                    reads++;
+                    frames[evict] = newPage;
+                }
+            }
+        }else if(rw == 'W'){
+            // look for the page, and if it is found, change its dirty bit
+            if(frames[previous_page].address == addr){
+                found = true;
+                frames[previous_page].dirtyBit = 0;
+                recent[previous_page] = count;
+                continue;
+            }
+
+            for(int i = 0; i < frame_size; i++){
+
+                // if we find the relevant page, change its dirty bit
+                // and set previous page to the index we found it at
+                // then the corresponding value in recent is set to count
+                // this is to take advantage of principle of locality
+                // to avoid iterating through the entire array for multiple
+                // reads of the same page
+                if(frames[i].address == addr){
+                    previous_page = i;
+                    recent[previous_page] = count;
+                    found = true;
+                    frames[previous_page].dirtyBit = 0;
+                    break;
+                }
+            }
+
+            // if the page was not found, evict a random page
+            // increment read, then change the dirty bit
+            if(!found){
+                int comp = count;
+                int evict;
+                // iterates through recent to find the position of the lowest value
+                for(int j = 0; j<frame_size;j++){
+                    if(recent[j] < comp){
+                        evict = j;
+                    }
+                }
+                
+                //int evict = rand() % frame_size;
+                Page newPage;
+                newPage.address = addr;
+                newPage.dirtyBit = 1;
+                // if the page to be evicted has been modified
                 // increment writes and read
                 if(frames[evict].dirtyBit == 1){
                     writes++;
